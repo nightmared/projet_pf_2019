52913a49ac084d69232bf7e3c71cc93196733e11
Corentin Lauverjat <corentin.lauverjat@etu.enseeiht.fr>
2020-01-15 18:09:43 +0100

    Gestion du score, affichage du # de balle restante (nb de vies), réécriture avec des enregistrements (record)

--- decl.ml
+++ decl.ml
@@ -1,7 +1,8 @@
 open Scheduler
+open Graphics
 
-type lifetime = int option
-type properties = NilProp
+type integer = | Int of int | Infinity;;
+type properties = {color: color; value: int}
 
 type position = int * int
 type floating_position = float * float
@@ -12,49 +13,26 @@ type vitesse_deplacement = float * float
 (* La brique a une durée de vie, et elle sera supprimée (à la charge de l'appelant)
 * lorsque celle-ci atteindra 'Some 0'.
 * Note: sa durée de vie peut aussi être infinie, avec 'None' *)
-type brique = Brique: position * lifetime * properties -> brique
+type brique = { position : position; lifetime : integer ; properties : properties};;
+
 
 
 (* une liste (probablement chaînée, suivant le niveau d'optimisation du compilateur),
 * bien que non idéale au niveau des performances, a
 * l'avantage de permettre des insertions/suppressions en O(1),
 * or ces opérations ont lieu fréquemment dans notre jeu *)
-type terrain = Terrain: brique list -> terrain
+type terrain = brique list
 
-type balle = Balle: floating_position * direction -> balle
+type balle = { pos: floating_position; direction : direction};;
 
-type raquette = Raquette: position * vitesse_deplacement -> raquette
+type raquette = { position: position; vitesse_deplacement: vitesse_deplacement};;
 
-type win_size = int * int
+type local_etat = { terrain : terrain ; balle : balle; raquette: raquette; nb_vies : int };;
 
-type nb_vies = int
+type global_etat = { window_size : int * int; score : int};;
 
-type local_etat = LocalState: terrain * balle * raquette -> local_etat
-(* TODO: handle resize ? *)
-type global_etat = GlobalState: win_size * nb_vies -> global_etat
-type etat = State: local_etat * global_etat -> etat
+type etat = {global_etat: global_etat; local_etat: local_etat;};;
 
-
-let raquette_get_pos (Raquette (pos, _)) = pos
-let raquette_get_speed (Raquette (_, speed)) = speed
-let balle_get_pos (Balle (pos, _)) = pos
-let balle_get_dir (Balle (_, dir)) = dir
-let brique_get_pos (Brique (pos, _, _)) = pos
-let etat_get_local (State (local, _)) = local
-let etat_get_global (State (_, global)) = global
-let etat_get_win_size etat = let GlobalState (win_size, _) = etat_get_global etat in win_size
-let etat_get_nb_vies etat = let GlobalState (_, nb_vies) = etat_get_global etat in nb_vies
-let etat_get_terrain etat = let LocalState (terrain, _, _) = etat_get_local etat in terrain
-let etat_get_balle etat = let LocalState (_, balle, _) = etat_get_local etat in balle
-let etat_get_raquette etat = let LocalState (_, _, raquette) = etat_get_local etat in raquette
-let etat_update_terrain (State ((LocalState (_, balle, raquette)), g_etat)) terrain =
-	State (LocalState (terrain, balle, raquette), g_etat)
-let etat_update_balle (State ((LocalState (terrain, _, raquette)), g_etat)) balle =
-	State (LocalState (terrain, balle, raquette), g_etat)
-let etat_update_raquette (State ((LocalState (terrain, balle, _)), g_etat)) raquette =
-	State (LocalState (terrain, balle, raquette), g_etat)
-let etat_update_nb_vies (State (local, GlobalState (win_size, _))) nb_vies =
-	(State (local, GlobalState (win_size, nb_vies)))
 
 (* Module d'ordonnancement coopératif des différentes tâches *)
 module GreenThreadsState = GreenThreads (struct type shift = etat end)
--- ui.ml
+++ ui.ml
@@ -20,19 +20,22 @@ let draw_rect (x, y) (w, h) = draw_rect x y w h
 let est_dans_rectangle (posx, posy) (rect_x, rect_y) (rect_width, rect_height) =
 	posx >= rect_x && posx <= rect_x + rect_width && posy >= rect_y && posy <= rect_y + rect_height
 
-let collision_rectangle_haut_droite balle rect rect_size =
-	est_dans_rectangle (tuple_to_int (add_tuple (balle_get_pos balle) (add_tuple (balle_get_dir balle) (balle_radius, balle_radius)))) rect rect_size
-let collision_rectangle_haut_gauche balle rect rect_size =
-	est_dans_rectangle (tuple_to_int (add_tuple (balle_get_pos balle) (add_tuple (balle_get_dir balle) (-.balle_radius, balle_radius)))) rect rect_size
-let collision_rectangle_bas_droite balle rect rect_size =
-	est_dans_rectangle (tuple_to_int (add_tuple (balle_get_pos balle) (add_tuple (balle_get_dir balle) (balle_radius, -.balle_radius)))) rect rect_size
-let collision_rectangle_bas_gauche balle rect rect_size =
-	est_dans_rectangle (tuple_to_int (add_tuple (balle_get_pos balle) (add_tuple (balle_get_dir balle) (-.balle_radius, -.balle_radius)))) rect rect_size
+let collision_rectangle_haut_droite (balle: balle) rect rect_size =
+	est_dans_rectangle (tuple_to_int (add_tuple balle.pos (add_tuple (balle.direction) (balle_radius, balle_radius)))) rect rect_size
+
+let collision_rectangle_haut_gauche (balle: balle) rect rect_size =
+	est_dans_rectangle (tuple_to_int (add_tuple balle.pos (add_tuple (balle.direction) (-.balle_radius, balle_radius)))) rect rect_size
+
+let collision_rectangle_bas_droite (balle : balle) rect rect_size =
+	est_dans_rectangle (tuple_to_int (add_tuple balle.pos (add_tuple (balle.direction) (balle_radius, -.balle_radius)))) rect rect_size
+
+let collision_rectangle_bas_gauche (balle : balle) rect rect_size =
+	est_dans_rectangle (tuple_to_int (add_tuple balle.pos (add_tuple (balle.direction) (-.balle_radius, -.balle_radius)))) rect rect_size
 
 (* On vérifie les collisions de la manière suivante (la version précédente faisait
  * du 'lancer de rayon' pour détecter les collisions, mais ce n'est pas très pratique
  * puisque notre balle ne se réduit pas à un seul point *)
-let collision_rectangle balle rect rect_size =
+let collision_rectangle (balle: balle) rect rect_size =
 	(* on vérifie les collisions avec les quatres extrêmes de la 'bounding box' du cercle *)
 	(collision_rectangle_bas_gauche balle rect rect_size
 	|| collision_rectangle_bas_droite balle rect rect_size
@@ -41,18 +44,18 @@ let collision_rectangle balle rect rect_size =
 
 
 
-let collision balle (Terrain terrain) =
+let collision balle (terrain : terrain) =
 	List.filter
-		(fun brique -> collision_rectangle balle (brique_get_pos brique) (brique_width, brique_height))
-		terrain
+		(fun  (brique : brique) -> collision_rectangle balle (brique.position) (brique_width, brique_height))
+		terrain;;
 
-let gen_brique x_idx y_idx _ height =
-	Brique (
-		(((x_idx + 1) * brique_border + (x_idx) * brique_width),
-		height-((y_idx + 1) * brique_border + (y_idx-1) * brique_height)),
-		Some 1,
-		NilProp
-	)
+let gen_brique x_idx y_idx _ height : brique =
+	{
+		position = (((x_idx + 1) * brique_border + (x_idx) * brique_width),
+		height-((y_idx + 1) * brique_border + (y_idx-1) * brique_height));
+		lifetime = Int 1 ; 
+		properties = { color = blue; value = 1}
+	};;
 
 (* TODO: ajouter de l'aléatoire là-dedans - c'est la raison d'être de l'utilisation
  * du type Option ici *)
@@ -63,7 +66,7 @@ let gen_terrain width height =
 	(* on réserve quelques lignes en bas pour que le jeu soit jouable *)
 	in let nb_briques_par_colonne = (height-brique_border)/(brique_border+brique_height) - 6
 	(* on conserve uniquement les éléments non nuls *)
-	in List.filter_map
+	in let liste_briques = List.filter_map
 		(fun x -> x)
 		(List.flatten
 			(List.init nb_briques_par_ligne
@@ -71,36 +74,56 @@ let gen_terrain width height =
 					(fun y_idx -> Some(gen_brique x_idx y_idx width height))
 				)
 			)
-		)
+		) in
+		liste_briques;;
 
-let etat_initial (size_win_x, size_win_y) =
+
+let etat_local_initial (size_win_x, size_win_y)  : local_etat =
 	let terrain = gen_terrain size_win_x size_win_y
-	in let balle = Balle ((float_of_int (size_win_x/2), (float_of_int raquette_height)+.balle_radius), (4.5, 4.5))
-	in let raquette = Raquette (((size_win_x-raquette_width)/2, 0), (0., 0.))
-	in State (LocalState (Terrain terrain, balle, raquette), GlobalState ((size_win_x, size_win_y), 3))
+	in let balle = { 
+		pos = float_of_int (size_win_x/2), (float_of_int raquette_height)+.balle_radius ; 
+		direction = (4.5, 4.5)
+	}
+	in let raquette = { 
+			position = ((size_win_x-raquette_width)/2, 0) ;
+			vitesse_deplacement = (0., 0.)
+	}
+	in { terrain = terrain ; balle = balle ; raquette = raquette; nb_vies = 3};;
+	
+
+let etat_initial window_size =
+	{ 
+		local_etat = etat_local_initial (window_size);
+		global_etat = { window_size = window_size; score = 0}
+	};;
+
 
 
 (* supprime une liste de blocs du terrain *)
-let supprimer_blocs (Terrain terrain) l =
-	Terrain
-		(List.fold_left (fun acc e -> List.filter_map (fun x -> if x = e then None else Some(x)) acc) terrain l)
+let supprimer_blocs (terrain : terrain) (l: brique list) =
+		List.fold_left (fun acc e -> 
+		List.filter_map (fun x -> if x = e then None else Some(x)) acc) 
+		terrain l;;
 
 
-let dessiner_terrain (Terrain liste_blocs) =
-	List.iter (fun brique ->
-		draw_rect (brique_get_pos brique) (brique_width, brique_height)
+let dessiner_terrain (liste_blocs : terrain) =
+	List.iter (fun (brique : brique) ->
+		let w, h = brique.position in
+		set_color brique.properties.color;
+		fill_rect w h brique_width brique_height;
+		set_color foreground;
 	) liste_blocs
 
-let dessiner_balle balle =
-	let (x, y) = tuple_to_int (balle_get_pos balle)
+let dessiner_balle (balle: balle) =
+	let (x, y) = tuple_to_int (balle.pos)
 	in draw_circle x y (int_of_float balle_radius)
 
-let dessiner_raquette raquette = draw_rect (raquette_get_pos raquette) (raquette_width, raquette_height)
+let dessiner_raquette (raquette: raquette) = draw_rect raquette.position (raquette_width, raquette_height)
 
-let avancer_balle balle etat =
-	let (x, y) = balle_get_pos balle
-	in let (dx, dy) = balle_get_dir balle
-	in let (win_size_x, win_size_y) = etat_get_win_size etat
+let avancer_balle (balle: balle) (etat: etat) : balle =
+	let (x, y) = balle.pos
+	in let (dx, dy) = balle.direction
+	in let (win_size_x, win_size_y) = etat.global_etat.window_size
 	in let (x, dx) =
 		(if x+.dx > (float_of_int win_size_x)-.balle_radius then
 			((float_of_int win_size_x)-.balle_radius, -.dx)
@@ -112,21 +135,27 @@ let avancer_balle balle etat =
 		((float_of_int win_size_y)-.balle_radius, -.dy)
 	else
 		(y+.dy, dy))
-	in (Balle ((x, y), (dx, dy)))
+	in  { pos = (x, y); direction = (dx, dy)}
 
 (* déplace la raquette si possible *)
-let deplacer_raquette (Raquette ((x, y), _)) win_size gauche =
+let deplacer_raquette ({ position = (x, y); _}) window_size gauche =
 	if gauche then
-		Raquette ((if x-raquette_offset < 0 then (0, y) else (x-raquette_offset, y)), (-.(float_of_int raquette_offset), 0.))
+		{
+			position = (if x-raquette_offset < 0 then (0, y) else (x-raquette_offset, y));
+			vitesse_deplacement = (-.(float_of_int raquette_offset), 0.)
+		}
 	else
-		Raquette ((if x+raquette_offset >= win_size then (win_size-raquette_width, y) else (x+raquette_offset, y)), (float_of_int raquette_offset, 0.))
+		{
+			position = (if x+raquette_offset >= window_size then (window_size-raquette_width, y) else (x+raquette_offset, y));
+			vitesse_deplacement = (float_of_int raquette_offset, 0.)
+		}
 
 (* modifie l'état en fonction des mouvements de la souris *)
-let gerer_entree_souris etat x =
+let gerer_entree_souris (etat: etat) x =
 	begin
-		let raquette = etat_get_raquette etat
-		in let (posx, _) = raquette_get_pos raquette
-		in let (win_size_x, _) = etat_get_win_size etat
+		let raquette = etat.local_etat.raquette
+		in let (posx, _) = raquette.position
+		in let (win_size_x, _) = etat.global_etat.window_size
 		in let new_raquette =
 			(if x > posx+raquette_width then
 				deplacer_raquette raquette win_size_x false
@@ -134,7 +163,7 @@ let gerer_entree_souris etat x =
 				deplacer_raquette raquette win_size_x true
 				else raquette)
 		in if new_raquette <> raquette then
-			etat_update_raquette etat new_raquette
+			{ etat with local_etat = { etat.local_etat with raquette = new_raquette} }
 		else
 			etat
 	end
@@ -157,43 +186,54 @@ let boucle_evenementielle () =
 	with Exit -> GreenThreadsState.exit ()
 
 (* détecte une collision entre la balle et la raquette, et fait avancer la balle *)
-let faire_evoluer_balle balle raquette etat =
-	let (posx, _) = balle_get_pos balle
-	in let (dirx, diry) = balle_get_dir balle
-	in if collision_rectangle balle (raquette_get_pos raquette) (raquette_width, raquette_height)
+let faire_evoluer_balle (balle: balle) (raquette: raquette) (etat: etat) =
+	let (posx, _) = balle.pos
+	in let (dirx, diry) = balle.direction
+	in if collision_rectangle balle raquette.position (raquette_width, raquette_height)
 	then
-		let (raquette_vitesse_x, _) = raquette_get_speed raquette
+		let (raquette_vitesse_x, _) = raquette.vitesse_deplacement
 		(* inversion de y et accélération de la vitesse de déplacement horizontale *)
-		in Balle ((posx+.dirx, (float_of_int raquette_height)+.balle_radius), (dirx+.raquette_vitesse_x/.10., -.diry))
+		in {
+			pos = (posx+.dirx, (float_of_int raquette_height)+.balle_radius);
+			direction = (dirx+.raquette_vitesse_x/.10., -.diry)
+		}
 	else
 		avancer_balle balle etat
 
 (* accélère légèrement la balle *)
-let accelerer_balle (Balle (pos, (dirx, diry))) =
-	Balle (pos, (min (dirx*.1.0005) 5., min (diry*.1.0005) 5.))
+let accelerer_balle ({pos = pos; direction = (dirx, diry)}: balle) =
+	{
+		pos = pos;
+		direction = (min (dirx*.1.0005) 5., min (diry*.1.0005) 5.)
+	};;
 
-let rebond_brique balle briques = 
-	if List.length briques > 0
-	then
-		let brique = List.hd briques
-		in let (posx, posy) = balle_get_pos balle
-		in let (dirx, diry) = balle_get_dir balle
-		in let (posbx, posby) = brique_get_pos brique
-		in if (collision_rectangle balle (posbx, posby) (brique_width, brique_height))
+let rebond_brique balle (briques: brique list) = 
+	match briques with 
+	| [] -> balle
+	| brique::_ -> 
+		let (posx, posy) = balle.pos
+		and (dirx, diry) = balle.direction
+		and (posbx, posby) = brique.position
+		in 
+		if (collision_rectangle balle (posbx, posby) (brique_width, brique_height))
 		then
 			if (posx >= (float_of_int (posbx+brique_width))) && (posy <= (float_of_int (posby+brique_height)) && (posy >= (float_of_int (posby))))
 				|| (posx <= (float_of_int (posbx)) && (posy <= (float_of_int (posby+brique_height))) && (posy >= (float_of_int (posby))))
 			(* Répartition de l'espace autour de la brique en 8 zones à la manière d'un quadrillage 3x3 (la brique au centre)
 			si la balle se trouve dans la zone directement à droite ou à gauche de la brique, alors rebond sur une surface verticale*)
 			then 
-				Balle ((posx-.dirx, posy+.diry), (-.dirx, diry))
+			 	{ 
+					pos = (posx-.dirx, posy+.diry);
+				  direction = (-.dirx, diry)
+				}
 			else (* sinon, rebond sur surface horizontale *)
-				Balle ((posx+.dirx, posy-.diry), (dirx, -.diry))
+				{
+					pos = (posx+.dirx, posy-.diry) ;
+					direction = (dirx, -.diry)
+				}
 			(* à l'heure actuelle, les zones correspondants aux coins de la brique ne sont pas traitées... *)
 		else (* pas de rebond, on re change rien *)
-			balle
-	else
-		balle
+			balle;;
 
 (* Fait avancer la balle, détecte les collisions et supprime les blocs détruits *)
 let detecter_collisions () =
@@ -209,13 +249,27 @@ let detecter_collisions () =
 				done;
 				let etat = GreenThreadsState.get ()
 				in begin
-					let balle = etat_get_balle etat
-					in let terrain = etat_get_terrain etat
+				  let local_state = etat.local_etat in
+					let balle = local_state.balle 
+					and terrain = local_state.terrain
+					and raquette = local_state.raquette 
 					(* détection et suppression des blocs sur le chemin de la balle *)
 					in let blocs_collisionants = collision balle terrain
-					in let balle = rebond_brique (accelerer_balle (faire_evoluer_balle balle (etat_get_raquette etat) etat)) blocs_collisionants
+					in let balle = rebond_brique (accelerer_balle (faire_evoluer_balle balle raquette etat)) blocs_collisionants
 					in let terrain = supprimer_blocs terrain blocs_collisionants
-					in GreenThreadsState.send (etat_update_balle (etat_update_terrain etat terrain) balle)
+					in let score = 
+					List.fold_left (+) etat.global_etat.score (List.map (fun (bloc:brique) -> 
+						bloc.properties.value
+					) blocs_collisionants) 
+					 
+					in let new_state = 
+					{  local_etat =  
+							{etat.local_etat with terrain = terrain ; balle = balle};
+						global_etat = 
+							{etat.global_etat with score = score}
+					}
+
+					in GreenThreadsState.send new_state
 				end;
 			end;
 	done; GreenThreadsState.exit ()
@@ -224,9 +278,10 @@ let detecter_fin_du_jeu () =
 	begin
 		while true do
 			let etat = GreenThreadsState.get ()
-			in let (_, y) = balle_get_pos (etat_get_balle (etat))
+			in let etat_local = etat.local_etat 
+			in let (_, y) = etat_local.balle.pos
 			in if y < 0. then
-				let nb_vies = etat_get_nb_vies etat
+				let nb_vies = etat_local.nb_vies
 				in (if nb_vies = 1 then
 					(* l'utilisateur a perdu *)
 					(* TODO: mieux gérer les cas où la vitesse permet au vecteur direction
@@ -235,8 +290,14 @@ let detecter_fin_du_jeu () =
 					GreenThreadsState.stop_scheduler ()
 				else
 					(* réinitialisation du jeu avec mise à jour du nombre de parties restantes *)
-					let etat = etat_update_nb_vies (etat_initial (etat_get_win_size etat)) (nb_vies-1)
-					in GreenThreadsState.send etat)
+					let new_etat =  {
+						etat with
+						local_etat = {
+							(etat_local_initial etat.global_etat.window_size) with 
+							nb_vies = nb_vies - 1
+						} 
+					}
+					in GreenThreadsState.send new_etat)
 			else
 				GreenThreadsState.yield ();
 		done;
@@ -245,16 +306,30 @@ let detecter_fin_du_jeu () =
 		GreenThreadsState.exit ()
 	end
 
+let dessiner_score score (w, h) = 
+	moveto (w/50) (h/50);
+	set_font "-*-fixed-medium-r-semicondensed--25-*-*-*-*-*-iso8859-1";
+	draw_string "Score : ";
+	draw_string (string_of_int score);;
+
+let dessiner_nb_vies nb_vie (w, h) = 
+moveto (w - 150) (h/50);
+set_font "-*-fixed-medium-r-semicondensed--25-*-*-*-*-*-iso8859-1";
+draw_string "# Vies : ";
+draw_string (string_of_int nb_vie);;
 (* Dessine les différents objets à l'écran *)
 let dessiner () =
 	begin
 		while true do
-			let LocalState (terrain, balle, raquette) = etat_get_local (GreenThreadsState.get ())
+			let etat  = (GreenThreadsState.get ()) 
+			in let etat_local = etat.local_etat 
 			in begin
 				clear_graph ();
-				dessiner_terrain terrain;
-				dessiner_balle balle;
-				dessiner_raquette raquette;
+				dessiner_terrain etat_local.terrain;
+				dessiner_balle etat_local.balle;
+				dessiner_raquette etat_local.raquette;
+				dessiner_score etat.global_etat.score etat.global_etat.window_size;
+				dessiner_nb_vies etat.local_etat.nb_vies etat.global_etat.window_size;
 				synchronize ();
 			end;
 			GreenThreadsState.yield ();
